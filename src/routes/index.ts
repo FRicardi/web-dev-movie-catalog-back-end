c0bb8a05069ea8d041c76a93fa1f2b19bcc37053
Felipe Ricardi <felipe.ricardi@sap.com>
2021-04-25 02:20:06 -0300

    adding review routes file to main router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import moviesRouter from './movies';
+import reviewsRouter from './reviews';
 
 const routes = Router();
 
 routes.use('/movies', moviesRouter);
+routes.use('/reviews', reviewsRouter);
 
 export default routes;
