e9d8de98644e93d1cbf5110623d1ce5912cb5477
Felipe Ricardi <felipe.ricardi@sap.com>
2021-06-20 02:03:49 -0300

    updating movie dependency

--- src/interfaces/movie.d.ts
+++ src/interfaces/movie.d.ts
@@ -1,14 +1,35 @@
 import { Review } from "./review";
 
+export interface MovieQuery {
+    limit?: number;
+    page?: number;
+    query_term?: string;
+    sort_by?: string;
+    order_by?: string;
+}
+
+export interface MovieList {
+    movies: Movie[];
+    page_number: number;
+    movie_count: number;
+    limit: number;
+}
+
 export interface Movie {
     id: number;
-    name: string;
-    type: string;
-    category: string;
-    producer: string;
-    director: string;
-    cast: string;
+    title: string;
     year: number;
+    genres: string[];
+    description_full: string;
+    language: string;
+    large_cover_image: string;
+    cast: CastMember[];
+    runtime: number;
     reviewAverage?: number;
     reviews: Review[];
+}
+
+export interface CastMember {
+    name: string;
+    character_name: string;
 }
