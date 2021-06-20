0e023806fddf48ad819a1ca4aed9a942377abdb3
Felipe Ricardi <felipe.ricardi@sap.com>
2021-06-20 02:05:08 -0300

    removing movie foreign key from reviews

--- src/database/migrations/20210425031440_reviews.ts
+++ src/database/migrations/20210425031440_reviews.ts
@@ -4,7 +4,7 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTableIfNotExists('reviews', (table: Knex.TableBuilder) => {
         table.increments();
-        table.bigInteger('movie_id').unsigned().index().references('id').inTable('movies')
+        table.bigInteger('movie_id');
         table.string('comment');
         table.integer('review');
     });
