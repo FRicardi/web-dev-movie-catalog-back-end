61289d99ab62feb2d405de15c1b084cfc8630ea7
Felipe Ricardi <felipe.ricardi@sap.com>
2021-06-21 17:50:22 -0300

    adding auth route to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import moviesRouter from './movies';
-import reviewsRouter from './reviews';
+import { authRouter } from './auth';
+import { moviesRouter } from './movies';
+import { reviewsRouter } from './reviews';
 
 const routes = Router();
 
 routes.use('/movies', moviesRouter);
+routes.use('/auth', authRouter);
 routes.use('/reviews', reviewsRouter);
 
 export default routes;
