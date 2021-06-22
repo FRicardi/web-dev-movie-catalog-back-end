555694593e2868d7205656866301b40ed4a52db8
Felipe Ricardi <felipe.ricardi@sap.com>
2021-06-21 22:00:29 -0300

    adding jwt guard on review posting

--- src/routes/reviews.ts
+++ src/routes/reviews.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { Review } from '../interfaces/review';
 import { reviewService } from '../services/reviews';
+import { userService } from '../services/users';
 
 const reviewsRouter = Router();
 
@@ -14,7 +15,7 @@ reviewsRouter.get('/:movie_id', async (request, response) => {
     }
 });
 
-reviewsRouter.post('/', async (request, response) => {
+reviewsRouter.post('/', userService.verifyJWT, async (request, response) => {
     try {
         const review = await reviewService.insert(request.body.review);
         return response.json({ review });
