b89007982dc26a5479d7b8a2d6b4ca17a421d848
Felipe Ricardi <felipe.ricardi@sap.com>
2021-06-21 21:59:15 -0300

    creating authentication service

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,10 +1,49 @@
 import { Router } from "express";
+import { CreateUser, GetUser } from "../interfaces/user";
 import { userService } from "../services/users";
 
 const authRouter = Router();
 
-authRouter.get('/', async (req, res) => {
-    return await userService.getUser({username: 'test', password: 'pass'});
+authRouter.post('/create', async (req, res) => {
+    try {
+        const userCredentials: CreateUser = {
+            username: req.body.username,
+            password: req.body.password
+        };
+
+        const createdUser = await userService.createUser(userCredentials);
+
+        const token = userService.loginUser(createdUser[0]);
+
+        return res.json({token});
+    } catch(e) {
+        console.log({e})
+        res.status(500);
+        res.send({error: e});
+    }
+});
+
+authRouter.post('/', async (req, res) => {
+    try {
+        const userCredentials: CreateUser = {
+            username: req.body.username,
+            password: req.body.password
+        };
+        const user: GetUser = await userService.getUser(userCredentials) as GetUser;
+        
+        if (user) {
+            const token = userService.loginUser(user.id);
+
+            return res.json({token});
+        } else {
+            res.status(404);
+            res.send({error: "User not found"});
+        }
+    } catch(e) {
+        console.log({e})
+        res.status(500);
+        res.send({error: e});
+    }
 });
 
 export { authRouter };
